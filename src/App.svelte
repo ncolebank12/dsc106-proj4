31c22516d9d7ef488e22d413cd53a357d570fee8
Nic Colebank <niccolebank@gmail.com>
2024-05-24 16:10:38 -0700

    switched to iris data, created initial scatterplot

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,29 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+  import KMeansGrapher from './components/KMeansGrapher.svelte';
+
+  let data = [];
+
+  onMount(async () => {
+    data = await d3.csv('dsc106-proj4/iris.csv', d3.autoType);
+    console.log(data);
+  });
+  
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <h1>Visualizing K-Means Clustering</h1>
+  <h3>An Unsupervised Machine Learning Algorithm</h3>
+
+  {#if data.length === 0}
+  <p>Loading...</p>
+  {:else}
+    <KMeansGrapher {data} />
+  {/if}
 
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
--- src/components/KMeansGrapher.svelte
+++ src/components/KMeansGrapher.svelte
@@ -1,7 +1,71 @@
 <script>
+    export let data;
     import * as d3 from 'd3';
+    import { onMount } from 'svelte';
+
+    const width = 928;
+    const height = 550;
+    const marginTop = 10;
+    const marginRight = 10;
+    const marginBottom = 20;
+    const marginLeft = 40;
+
+    const innerHeight = height - marginBottom - marginTop;
+    const innerWidth = width - marginLeft - marginRight;
+
+    const xVar = 'Sepal_Length';
+    const yVar = 'Petal_Width';
+
+    function createScatter() {
+        const svg = d3.select('#scatter')
+            .attr('width', width)
+            .attr('height', height);
+        console.log(svg);
+        svg.selectAll('*').remove(); // Clear previous content
+        const x = d3.scaleLinear()
+            .domain(d3.extent(data, (d) => d[xVar]))
+            .range([0, innerWidth]);
+
+        const y = d3.scaleLinear()
+            .domain([0,2.8])
+            .range([innerHeight, 0]);
+        
+        const g = svg.append('g')
+            .attr('transform', `translate(${marginLeft},${marginTop})`);
+
+        g.append('g')
+            .call(d3.axisLeft(y));
+
+        g.append('g')
+            .attr('transform', `translate(0,${innerHeight})`)
+            .call(d3.axisBottom(x));
+
+        svg.append('g')
+            .attr('transform', `translate(${marginLeft},0)`)
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return x(d[xVar]); } )
+            .attr("cy", function (d) { return y(d[yVar]); } )
+            .attr("r", 5)
+            .style("fill", "#69b3a2")
+    }
+
+    onMount(() => {
+        createScatter();
+    })
 
 
+    
 </script>
 
 
+<svg id='scatter'></svg>
+
+<style>
+    svg {
+        max-width: 100%;
+        height: auto;
+    }
+</style>
